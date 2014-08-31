9eae12e2f68d03036350cc5265c0725818e02618
steachens14 <stj217@lehigh.edu>
2014-08-31 19:35:36 +0000

    Updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Steven Jodogne
+//Hw01
+//Welcome Class Java Program
+//  define a class
+public class WelcomeClass{ 
+
+    //  add main method
+    public static void main(String[] args)  {
+    
+    //  print the assigned image
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("    ^  ^  ^  ^  ^  ^  ");
+    System.out.println("   / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("  |-S--T--J--2--1--7-|");
+    System.out.println("   \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("    v  v  v  v  v  v");
+    System.out.println("I love travelling all over the world. Last summer I went to Paris,Nice,Florence, and Rome");
+    
+    }
+}
